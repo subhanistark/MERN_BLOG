d128f5b7c1b920882b2e24a552e43b3de2e12551
Subhani Stark <smartboysubani2@gmail.com>
2024-09-11 12:55:36 +0530

    created the Header Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Dashboard from './pages/Dashboard'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Header from './components/Header'
 
-const App = () => {
+export default function App() {
   return (
     <div>
-      <div className="bg-green-400">Hello World</div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
 
-export default App
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,52 @@
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon } from "react-icons/fa"
+
+export default function Header() {
+    const path = useLocation().pathname;
+    return (
+        <div>
+            <Navbar className="border-b-2">
+                <Link to="/" className="text-sm whitespace-nowrap sm:text-xl font-semibold dark:text-white">
+                    <span className="px-2 py-1 bg-gradient-to-r from-blue-500 via-fuchsia-500 to-red-500 rounded-lg text-white">Stark </span>
+                    Blog
+                </Link>
+                <form >
+                    <TextInput
+                        type="text"
+                        placeholder="search..."
+                        rightIcon={AiOutlineSearch}
+                        className="hidden lg:inline"
+                    />
+                </form>
+                <Button className="w-12 h-10 lg:hidden" color='gray' pill>
+                    <AiOutlineSearch />
+                </Button>
+                <div className="flex gap-2 md:order-2">
+                    <Button className="w-12 h-10" color='gray' pill>
+                        <FaMoon />
+                    </Button>
+                    <Link to="/SignIn">
+                        <Button gradientDuoTone='purpleToBlue' outline >
+                            Sign In
+                        </Button>
+                    </Link>
+                    <Navbar.Toggle />
+                </div>
+                <Navbar.Collapse>
+                    <Navbar.Link active={path === '/'} as={'div'}>
+                        <Link to={'/'}>Home</Link>
+                    </Navbar.Link>
+                    <Navbar.Link active={path === '/About'} as={'div'}>
+                        <Link to={'/About'}>About</Link>
+                    </Navbar.Link>
+                    <Navbar.Link active={path === '/Projects'} as={'div'}>
+                        <Link to={'/Projects'} >Projects</Link>
+                    </Navbar.Link>
+                </Navbar.Collapse>
+
+            </Navbar>
+        </div >
+    )
+}   
