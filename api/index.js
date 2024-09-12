9c1b80dcc17334a8a52e9107c6bc708dbf944699
Subhani Stark <smartboysubani2@gmail.com>
2024-09-12 11:51:41 +0530

    Connected to the Database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import Express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 const app = Express();
 
-app.listen( 8080,() => {
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO_URl)
+.then(()=>{
+    console.log("MongoDb is connected");
+}).catch(err=>{
+    console.log(err);
+});
+
+app.listen(8080, () => {
     console.log(`app is listening on port 8080`)
 })
